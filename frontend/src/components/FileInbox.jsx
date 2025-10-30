7f78e2421945a5eed0e213466062575593ed7063
Sreelekshmi S <sreelekshmi.s@quinoid.in>
2025-10-30 17:09:51 +0530

    useref change

--- frontend/src/components/FileInbox.jsx
+++ frontend/src/components/FileInbox.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import DeleteModal from './DeleteModal';
 import { useNavigate } from 'react-router-dom';
 import Sidebar from './Sidebar';
